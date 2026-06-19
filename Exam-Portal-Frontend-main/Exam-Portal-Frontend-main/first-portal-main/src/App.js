378fd93c74ea25545caf77aad7bbe547c451e12f
NARASIMHA131415 <narasimhareddy131415@gmail.com>
2026-06-19 07:20:49 +0530

    Update App.js

--- Exam-Portal-Frontend-main/Exam-Portal-Frontend-main/first-portal-main/src/App.js
+++ Exam-Portal-Frontend-main/Exam-Portal-Frontend-main/first-portal-main/src/App.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
-import Dashboard from './components/Dashboard';
+import Dashboard from './components/Dashboard'; 
 import ExamPortal from './components/ExamPortal';
 import ExamDashboard from './components/ExamDashboard';
 import ResultPage from './components/ResultPage';
