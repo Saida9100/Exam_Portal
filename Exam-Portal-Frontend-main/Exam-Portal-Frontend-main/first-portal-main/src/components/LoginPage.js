6df5f6958ddc30e13c67ce59b3b3000c391417ba
NARASIMHA131415 <narasimhareddy131415@gmail.com>
2026-06-19 10:16:54 +0530

    Update LoginPage.js

--- Exam-Portal-Frontend-main/Exam-Portal-Frontend-main/first-portal-main/src/components/LoginPage.js
+++ Exam-Portal-Frontend-main/Exam-Portal-Frontend-main/first-portal-main/src/components/LoginPage.js
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Spinner } from 'react-bootstrap';
-import apiService from '../services/api';
+import apiService from '../services/api'; 
 
 const LoginPage = () => {
   const navigate = useNavigate();
